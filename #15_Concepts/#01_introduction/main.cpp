31691debbc33b5040e69e5d281862d6dd1e6a12f
SSK <100ravsinghkarmwar@gmail.com>
2024-08-26 12:00:18 +0530

    feat: Add examples of C++20 Concepts with notes
    - Added introductory examples of C++20 Concepts in multiple files under the `#15_Concepts` directory.
      - Created `main.cpp` files for the following sections:
        - Introduction to Concepts
        - Using Concepts
        - Building Your Own Concepts
        - Requires Clause
        - Logical Combinations of Concepts
      - Included explanatory comments in the code to illustrate the use of concepts such as `same_as`, `derived_from`, `integral`, `floating_point`, and custom-built concepts like `MyIntegral`, `TinyType`, and `Addable`.
    - Updated the README.md with the new timeline: 20:04:11.

---
+++ #15_Concepts/#01_introduction/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// NOTE: Concepts : A mechanism to place constraints on your template type
+// parameters
+
+/* NOTE:
+ *  Core language concepts
+ *  # same_as : specifies that a type is the same as another type
+ *  # derived_from : specifies that a type is derived from another type
+ *  # convertible_to : specifies that a type is implicitly convertible to
+ *                     another type
+ * # common_reference_with : specifies that two types share a
+ *                           common reference type
+ * # common_with : specifies that two types share a common type
+ * # integral : specifies that a type is an integral type
+ * # signed_integral : specifies that a type is an integral type that is signed
+ * # unsigned_integral : specifies that a type is an integral type that is
+ *                       unsigned
+ * # floating_point : specifies that a type is a floating-point type
+ */
+
+int main() {
+  cout << endl;
+
+  return 0;
+}
